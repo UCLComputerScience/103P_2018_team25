48a2ba33e4c1ce0056e7dda60a26e2d0575e8aa2
HarryThomas17 <Harry.Thomas.17@ucl.ac.uk>
2018-02-25 16:34:24 +0000

    Added basic views and page structure

--- ixn/matchingsystem/urls.py
+++ ixn/matchingsystem/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'matchingsystem'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^student', views.student_form, name='student_form'),
+    url(r'^client', views.client_form, name='client_form'),
+    url(r'^results', views.results, name='results'),
 ]
--- ixn/matchingsystem/views.py
+++ ixn/matchingsystem/views.py
@@ -1,4 +1,24 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student, Project, Tag
 
 def index(request):
-    return HttpResponse("Matching system index.")
+    # Just displays all data from table
+    students = Student.objects.all()
+    projects = Project.objects.all()
+    tags = Tag.objects.all()
+    context = {
+        'students': students,
+        'projects': projects,
+        'tags': tags
+    }
+    return render(request, 'matchingsystem/index.html', context)
+
+def student_form(request):
+    return HttpResponse("Student form index")
+
+def client_form(request):
+    return HttpResponse("Client form index")
+
+def results(request):
+    return HttpResponse("Results page index")
