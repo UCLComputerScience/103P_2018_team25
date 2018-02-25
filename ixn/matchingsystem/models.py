fa016f7f3242884ab0f245a27d13c8ad1afca49f
HarryThomas17 <Harry.Thomas.17@ucl.ac.uk>
2018-02-25 12:18:11 +0000

    Added tag model

--- ixn/matchingsystem/models.py
+++ ixn/matchingsystem/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# These are the models to define database structure, they can be easily changed and the database will reflect this
+# So makes adding / removing features easy
+
+class Tag(models.Model):
+    # Default primary key is provided
+    tag_description = models.CharField(max_length=100) # Length to be changed
