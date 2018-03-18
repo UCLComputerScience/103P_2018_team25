024b347630dde99029604946c90697f23471f816
HarryThomas17 <Harry.Thomas.17@ucl.ac.uk>
2018-03-18 19:16:49 +0000

    Added static files to deploy

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,19 @@ echo "Installing requirements"
 
 python -m pip install -r requirements.txt -q
 
+echo "Collecting static files"
+
+python ixn/manage.py collectstatic --noinput --clear
+
+echo "Cleaning up deployment target"
+
+rm -r $DEPLOYMENT_TARGET/ssig_site
+
 echo "Running migrations"
 
 python ixn/manage.py migrate
 
 echo "Copying files to deployment target"
 
+cp -R ixn $DEPLOYMENT_TARGET/
 cp web.config $DEPLOYMENT_TARGET/
